22b1526d7d9d4bb960aba1200debda6886ab04dd
lvnchboxx <rangga.aulia@icloud.com>
2022-12-16 07:35:04 +0700

    .

--- layexo.php
+++ layexo.php
@@ -126,7 +126,7 @@
                             <div class="row sn-slider">
                                 <div class="col-md-4">
                                     <div class="sn-img">
-                                        <img src="img/news-825x825.jpg" />
+                                        <img src="img/kaesang.jpg" />
                                         <div class="sn-title">
                                             <a href="Entertaiment.php">Komentar Kocak Kaesang Saat Tahu Pernikahannya Viral</a>
                                         </div>
@@ -281,16 +281,8 @@
         
         <div id="disqus_thread"></div>
 <script>
-    /**
-    *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
-    *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables    */
-    /*
-    var disqus_config = function () {
-    this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
-    this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-    };
-    */
-    (function() { // DON'T EDIT BELOW THIS LINE
+   
+    (function() { 
     var d = document, s = d.createElement('script');
     s.src = 'https://itsnews.disqus.com/embed.js';
     s.setAttribute('data-timestamp', +new Date());
